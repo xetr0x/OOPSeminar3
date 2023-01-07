2723040a97d42473862bdb4dc540cb63f4db4836
Kevin Wilusz <kwilusz10@gmail.com>
2023-01-07 17:10:02 +0100

    proceeding to doing task3

--- src/se/hkr/task3/MyArrayList.java
+++ src/se/hkr/task3/MyArrayList.java
@@ -1,4 +1,30 @@
 package se.hkr.task3;
+import java.util.Arrays;
 
-public class MyArrayList {
+
+public class MyArrayList<T> {
+    T Object;
+    T[] MyArray;
+    private int index = 0;
+    private int indexSeries = 0;
+
+    @SuppressWarnings("unchecked")
+    public MyArrayList(){ MyArray = (T[]) new Object[10];
+    }
+
+    public void add(T Object){
+        if(index == 10){
+            index = 0;
+            indexSeries++;
+            MyArray = Arrays.copyOf(MyArray, index + 10);
+            MyArray[index] = Object;
+        }else{
+            MyArray[index] = Object;
+            index++;
+        }
+    }
+
+    public T get(){
+        return x;
+    }
 }
